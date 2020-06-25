57ec3458f030c29796b040cf1e9826c4660d6956
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-06-25 12:58:26 +0200

    Minimum version of Symfony is ^5.1.

---
+++ src/Resources/config/services.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use EcPhp\Ecas\Ecas;
+use EcPhp\Ecas\EcasProperties;
+use EcPhp\EuLoginBundle\Security\Core\User\EuLoginUserProvider;
+
+return static function (ContainerConfigurator $container) {
+    $container
+        ->services()
+        ->set('eulogin.userprovider', EuLoginUserProvider::class);
+
+    $container
+        ->services()
+        ->set('ecas.configuration', EcasProperties::class)
+        ->decorate('cas.configuration')
+        ->args([
+            service('ecas.configuration.inner'),
+            service('request_stack'),
+        ]);
+
+    $container
+        ->services()
+        ->set('ecas', Ecas::class)
+        ->decorate('cas')
+        ->args([
+            service('cas.inner'),
+            service('nyholm.psr7.psr17_factory'),
+        ]);
+};
