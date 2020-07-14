3767a11aca80c73e7f49115cfb3b5539d2c8c8e0
Pol Dellaiera <pol.dellaiera@protonmail.com>
2020-07-14 15:15:06 +0200

    Use decorator pattern for the User Provider.

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -6,12 +6,24 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use EcPhp\Ecas\Ecas;
 use EcPhp\Ecas\EcasProperties;
+use EcPhp\Ecas\Introspection\EcasIntrospector;
 use EcPhp\EuLoginBundle\Security\Core\User\EuLoginUserProvider;
 
 return static function (ContainerConfigurator $container) {
     $container
         ->services()
-        ->set('eulogin.userprovider', EuLoginUserProvider::class);
+        ->set('ecas.introspector', EcasIntrospector::class)
+        ->decorate('cas.introspector')
+        ->args([
+            ref('ecas.introspector.inner'),
+        ]);
+
+    $container
+        ->services()
+        ->set('eulogin.userprovider', EuLoginUserProvider::class)
+        ->args([
+            ref('cas.userprovider'),
+        ]);
 
     $container
         ->services()
