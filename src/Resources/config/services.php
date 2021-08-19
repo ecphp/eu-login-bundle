6d068e4e510fb338da90a481ae46f4fd51937086
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-08-19 11:47:21 +0200

    refactor: Update Symfony services definitions.

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -17,36 +17,29 @@ use EcPhp\Ecas\Introspection\EcasIntrospector;
 use EcPhp\EuLoginBundle\Security\Core\User\EuLoginUserProvider;
 
 return static function (ContainerConfigurator $container) {
-    $container
-        ->services()
+    $services = $container->services();
+
+    $services
+        ->defaults()
+        ->autoconfigure(true)
+        ->autowire(true);
+
+    $services
         ->set('ecas.introspector', EcasIntrospector::class)
         ->decorate('cas.introspector')
-        ->args([
-            service('ecas.introspector.inner'),
-        ]);
+        ->arg('$introspector', service('ecas.introspector.inner'));
 
-    $container
-        ->services()
+    $services
         ->set('eulogin.userprovider', EuLoginUserProvider::class)
-        ->args([
-            service('cas.userprovider'),
-        ]);
+        ->arg('$casUserProvider', service('cas.userprovider'));
 
-    $container
-        ->services()
+    $services
         ->set('ecas.configuration', EcasProperties::class)
         ->decorate('cas.configuration')
-        ->args([
-            service('ecas.configuration.inner'),
-            service('request_stack'),
-        ]);
+        ->arg('$casProperties', service('ecas.configuration.inner'));
 
-    $container
-        ->services()
+    $services
         ->set('ecas', Ecas::class)
         ->decorate('cas')
-        ->args([
-            service('ecas.inner'),
-            service('nyholm.psr7.psr17_factory'),
-        ]);
+        ->arg('$cas', service('ecas.inner'));
 };
