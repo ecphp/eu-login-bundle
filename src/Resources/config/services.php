d3cfd3e2f1060c65b811a41b33e6b9a62539f413
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-04-21 10:58:27 +0200

    Fix declared services.

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -11,12 +11,13 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use EcPhp\CasBundle\Security\Core\User\CasUserProvider;
 use EcPhp\CasLib\Contract\CasInterface;
 use EcPhp\CasLib\Contract\Configuration\PropertiesInterface;
-use EcPhp\CasLib\Contract\Response\CasResponseBuilderInterface;
+use EcPhp\CasLib\Contract\Response\Factory\ServiceValidateFactory as FactoryServiceValidateFactory;
 use EcPhp\Ecas\Ecas;
 use EcPhp\Ecas\EcasProperties;
-use EcPhp\Ecas\Response\EcasResponseBuilder;
+use EcPhp\Ecas\Response\Factory\ServiceValidateFactory;
 use EcPhp\EuLoginBundle\Security\Core\User\EuLoginUserProvider;
 
 return static function (ContainerConfigurator $container) {
@@ -28,18 +29,18 @@ return static function (ContainerConfigurator $container) {
         ->autowire(true);
 
     $services
-        ->set(EcasResponseBuilder::class)
-        ->decorate(CasResponseBuilderInterface::class)
-        ->arg('$casResponseBuilder', service('.inner'));
-
-    $services
-        ->set('eulogin.userprovider', EuLoginUserProvider::class)
-        ->arg('$casUserProvider', service('cas.userprovider'));
+        ->set(EuLoginUserProvider::class)
+        ->arg('$casUserProvider', service(CasUserProvider::class));
 
     $services
         ->set(EcasProperties::class)
         ->decorate(PropertiesInterface::class)
         ->arg('$casProperties', service('.inner'));
+
+    $services
+        ->set(ServiceValidateFactory::class)
+        ->decorate(FactoryServiceValidateFactory::class)
+        ->arg('$serviceValidateFactory', service('.inner'));
 
     $services
         ->set(Ecas::class)
